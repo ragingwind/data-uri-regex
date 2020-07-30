a9d7474c833e8fbf5b1821fe65d8cccd6aea4536
OEMarshall <ochaun@secureideas.com>
2020-07-30 02:41:30 +0000

    Regex edits and corresponding unit tests

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-'use strict';
+"use strict";
 
-module.exports = function () {
+module.exports = function() {
 	// data-uri scheme
 	// data:[<media type>][;charset=<character set>][;base64],<data>
-	return new RegExp(/^(data:)([\w\/\+]+);(charset=[\w-]+|base64).*,(.*)/gi);
+	return new RegExp(/^(data:)([\w\/\+-]*)(;charset=[\w-]+|;base64){0,1},(.*)/gi);
 };
